8a73228fdd794dd868b5959c73cff9d4f759b90d
Stephen Fang <safang99@gmail.com>
2020-01-13 14:55:49 -0500

    initial seed data

--- schema.sql
+++ schema.sql
@@ -12,3 +12,9 @@ CREATE TABLE articles (
   description TEXT,
   url VARCHAR(255) NOT NULL
 );
+
+INSERT INTO articles(title, description, url)
+VALUES
+  ('Best dumplings in Boston', 'What it sounds like', 'www.boutdatbao.com'),
+  ('Pizza time', 'Where to go to get a great slice', 'www.ernestos.com'),
+  ('Spiciest dishes', 'The most painful dishes to eat in the city', 'www.eastcoasthellnightrip.com');
